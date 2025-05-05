26c9cf3f87a039db6fa25f15285ad383b76af0b6
UWDigitalAg <65968018+UWDigitalAg@users.noreply.github.com>
2025-05-05 12:14:16 -0500

    Update simple_client.py
    Updated with example on how to download files to local machine programmatically in simple_client.py

--- simple_client.py
+++ simple_client.py
@@ -6,6 +6,7 @@ Created on Fri Apr  3 20:56:24 2020
 """
 
 # Core
+import asyncio
 import logging
 from logging.handlers import RotatingFileHandler
 import time
@@ -13,6 +14,7 @@ import traceback
 import shutil
 import sys
 import os
+import json
 
 # Third Party
 import requests
@@ -23,6 +25,14 @@ if getattr(sys, 'frozen', False):
     Current_Path = os.path.dirname(sys.executable)
 else:
     Current_Path = str(os.path.dirname(__file__))
+
+# load credentials
+with open("parameters.json", "r") as f:
+    parameters = json.load(f)
+    PASS = parameters["password"]
+    LOGIN = parameters["login"]
+    DOWNLOAD_DIR = parameters["download"]
+    SAMPLE_DIR = parameters["sample"]
 
 # Setup logging
 logger = logging.getLogger("Client")
@@ -215,123 +225,159 @@ if __name__ == '__main__':
         endpoint=USER_URL,
         url="login",
         req_data={
-            "username": "",
-            "password": ""
+            "username": LOGIN,
+            "password": PASS
         },
         parameters=None
     )
     # do things
-    # In this example getting an overview of plants per months
+    #
+    """Example 1: download some data"""
+    # Prepare request:
     import datetime
-    import pandas as pd
-    month_row = []
-    for year in [2020, 2021]:
-        for month in range(1, 13):
-            month_row.append(str(year)+"/"+str(month))
-    for year in [2022]:
-        for month in range(1, 9):
-            month_row.append(str(year)+"/"+str(month))
-    df = pd.DataFrame({"Month": month_row})
 
-    import json
-    with open("metadata.json", "r") as f:
-        metadata = json.load(f)
+    start_date = datetime.datetime(2021, 4, 1, 0, 0, 0)
+    end_date = datetime.datetime(2021, 4, 3, 0, 0, 0)
 
-    for plant in metadata["eagli_plants"]:
-        plant_row = []
-        for year in [2020, 2021]:
-            for month in range(1, 12):
-                start_date = datetime.datetime(year, month, 1, 0, 0, 0)
-                end_date = datetime.datetime(year, month+1, 1, 0, 0, 0)
+    req_data = {
+        'eagli_parameters.start_date': start_date,
+        'eagli_parameters.end_date': end_date,
+        'eagli_parameters.min_age': 0,
+        'eagli_parameters.max_age': 1000,
+        'eagli_parameters.plants': "FieldPea",
+        'eagli_parameters.plant_id': '',
+        'eagli_parameters.min_res': 0,
+        'eagli_parameters.max_res': 4000,
+        'eagli_parameters.single_plant_output': True,
+        'eagli_parameters.multiple_plant_output': False,
+        'eagli_parameters.bounding_box_output': False,
+        'eagli_parameters.json_output': False,
+        'eagli_parameters.archive_selection': '',
+        'eagli_parameters.perspectives': 'Any',
+        'eagli_parameters.custom_query_string': '',
+        'field_parameters.start_date': start_date,
+        'field_parameters.end_date': end_date,
+        'field_parameters.plants': '',
+        'field_parameters.archive_selection': '',
+        'field_parameters.custom_query_string': '',
+        'dataset': 'combined_images',
+        'sample_size': 0
+    }
+    response = send_req(
+        method="POST",
+        endpoint=API_URL,
+        url="check",
+        req_data=req_data,
+        parameters=None
+    )
+    print(response["message"])
 
-                req_data = {
-                    'eagli_parameters.start_date': start_date,
-                    'eagli_parameters.end_date': end_date,
-                    'eagli_parameters.min_age': 0,
-                    'eagli_parameters.max_age': 1000,
-                    'eagli_parameters.plants': plant,
-                    'eagli_parameters.plant_id': '',
-                    'eagli_parameters.min_res': 0,
-                    'eagli_parameters.max_res': 4000,
-                    'eagli_parameters.single_plant_output': True,
-                    'eagli_parameters.multiple_plant_output': False,
-                    'eagli_parameters.bounding_box_output': False,
-                    'eagli_parameters.json_output': False,
-                    'eagli_parameters.archive_selection': '',
-                    'eagli_parameters.perspectives': 'Any',
-                    'eagli_parameters.custom_query_string': '',
-                    'field_parameters.start_date': start_date,
-                    'field_parameters.end_date': end_date,
-                    'field_parameters.plants': '',
-                    'field_parameters.archive_selection': '',
-                    'field_parameters.custom_query_string': '',
-                    'dataset': 'combined_images',
-                    'sample_size': 20
-                }
-                data = send_req(
-                    method="POST",
-                    endpoint=API_URL,
-                    url="check",
-                    req_data=req_data,
-                    parameters=None
+    if data["message"] != "This query yielded no results.":
+        # Ask to prepare data
+        response = send_req(
+            method="POST",
+            endpoint=API_URL,
+            url="prepare",
+            req_data=req_data,
+            parameters=None
+        )
+
+        if "error" in response.keys():
+            print(response["error"])
+        else:
+            file_list = response["file_list"]
+            job_id = response["job_id"]
+            number_files = response["number_files"]
+            parts = response["parts"]
+
+            # Download each part into download directory
+            for part in range(int(parts)):
+                # Download part
+                asyncio.run(
+                    get_files(part, job_id, DOWNLOAD_DIR)
                 )
-                # Process response data
-                print("returned data is: ", data["message"])
-                if "error" in data.keys():
-                    plant_row.append(data["error"])
-                elif "message" in data.keys() and data["message"] == "This query yielded no results.":
-                    plant_row.append(0)
-                else:
-                    plant_row.append(data["number_files"])
+                # Acknowledge download at server. Otherwise server will not
+                # continue to prepare new parts for you!
+                parameters = {
+                    "job_id": job_id,
+                    "part_number": part,
+                    "final_part": (part == parts - 1)
+                }
+                send_req(
+                    method="GET",
+                    endpoint=API_URL,
+                    url="ack_download",
+                    req_data=None,
+                    parameters=parameters
+                )
 
-                # Return response data and gui-formatted response
-            # December
-            start_date = datetime.datetime(year, 12, 1)
-            end_date = datetime.datetime(year+1, 1, 1)
-
-            req_data = {
-                'eagli_parameters.start_date': start_date,
-                'eagli_parameters.end_date': end_date,
-                'eagli_parameters.min_age': 0,
-                'eagli_parameters.max_age': 1000,
-                'eagli_parameters.plants': plant,
-                'eagli_parameters.plant_id': '',
-                'eagli_parameters.min_res': 0,
-                'eagli_parameters.max_res': 4000,
-                'eagli_parameters.single_plant_output': True,
-                'eagli_parameters.multiple_plant_output': False,
-                'eagli_parameters.bounding_box_output': False,
-                'eagli_parameters.json_output': False,
-                'eagli_parameters.archive_selection': '',
-                'eagli_parameters.perspectives': 'Any',
-                'eagli_parameters.custom_query_string': '',
-                'field_parameters.start_date': start_date,
-                'field_parameters.end_date': end_date,
-                'field_parameters.plants': '',
-                'field_parameters.archive_selection': '',
-                'field_parameters.custom_query_string': '',
-                'dataset': 'combined_images',
-                'sample_size': 20
-            }
-            data = send_req(
-                method="POST",
-                endpoint=API_URL,
-                url="check",
-                req_data=req_data,
-                parameters=None
-            )
-            # Process response data
-            print("returned data is: ", data["message"])
-            if "error" in data.keys():
-                plant_row.append(data["error"])
-            elif "message" in data.keys() and data["message"] == "This query yielded no results.":
-                plant_row.append(0)
-            else:
-                plant_row.append(data["number_files"])
+    """ Example 2: In this example getting an overview of plants per months
+        import datetime
+        import pandas as pd
+        month_row = []
+        for year in [2020, 2021]:
+            for month in range(1, 13):
+                month_row.append(str(year)+"/"+str(month))
         for year in [2022]:
             for month in range(1, 9):
-                start_date = datetime.datetime(year, month, 1, 0, 0, 0)
-                end_date = datetime.datetime(year, month+1, 1, 0, 0, 0)
+                month_row.append(str(year)+"/"+str(month))
+        df = pd.DataFrame({"Month": month_row})
+
+        import json
+        with open("metadata.json", "r") as f:
+            metadata = json.load(f)
+
+        for plant in metadata["eagli_plants"]:
+            plant_row = []
+            for year in [2020, 2021]:
+                for month in range(1, 12):
+                    start_date = datetime.datetime(year, month, 1, 0, 0, 0)
+                    end_date = datetime.datetime(year, month+1, 1, 0, 0, 0)
+
+                    req_data = {
+                        'eagli_parameters.start_date': start_date,
+                        'eagli_parameters.end_date': end_date,
+                        'eagli_parameters.min_age': 0,
+                        'eagli_parameters.max_age': 1000,
+                        'eagli_parameters.plants': plant,
+                        'eagli_parameters.plant_id': '',
+                        'eagli_parameters.min_res': 0,
+                        'eagli_parameters.max_res': 4000,
+                        'eagli_parameters.single_plant_output': True,
+                        'eagli_parameters.multiple_plant_output': False,
+                        'eagli_parameters.bounding_box_output': False,
+                        'eagli_parameters.json_output': False,
+                        'eagli_parameters.archive_selection': '',
+                        'eagli_parameters.perspectives': 'Any',
+                        'eagli_parameters.custom_query_string': '',
+                        'field_parameters.start_date': start_date,
+                        'field_parameters.end_date': end_date,
+                        'field_parameters.plants': '',
+                        'field_parameters.archive_selection': '',
+                        'field_parameters.custom_query_string': '',
+                        'dataset': 'combined_images',
+                        'sample_size': 20
+                    }
+                    data = send_req(
+                        method="POST",
+                        endpoint=API_URL,
+                        url="check",
+                        req_data=req_data,
+                        parameters=None
+                    )
+                    # Process response data
+                    print("returned data is: ", data["message"])
+                    if "error" in data.keys():
+                        plant_row.append(data["error"])
+                    elif "message" in data.keys() and data["message"] == "This query yielded no results.":
+                        plant_row.append(0)
+                    else:
+                        plant_row.append(data["number_files"])
+
+                    # Return response data and gui-formatted response
+                # December
+                start_date = datetime.datetime(year, 12, 1)
+                end_date = datetime.datetime(year+1, 1, 1)
 
                 req_data = {
                     'eagli_parameters.start_date': start_date,
@@ -372,12 +418,55 @@ if __name__ == '__main__':
                     plant_row.append(0)
                 else:
                     plant_row.append(data["number_files"])
+            for year in [2022]:
+                for month in range(1, 9):
+                    start_date = datetime.datetime(year, month, 1, 0, 0, 0)
+                    end_date = datetime.datetime(year, month+1, 1, 0, 0, 0)
 
-        # Write row
-        df[plant] = plant_row
+                    req_data = {
+                        'eagli_parameters.start_date': start_date,
+                        'eagli_parameters.end_date': end_date,
+                        'eagli_parameters.min_age': 0,
+                        'eagli_parameters.max_age': 1000,
+                        'eagli_parameters.plants': plant,
+                        'eagli_parameters.plant_id': '',
+                        'eagli_parameters.min_res': 0,
+                        'eagli_parameters.max_res': 4000,
+                        'eagli_parameters.single_plant_output': True,
+                        'eagli_parameters.multiple_plant_output': False,
+                        'eagli_parameters.bounding_box_output': False,
+                        'eagli_parameters.json_output': False,
+                        'eagli_parameters.archive_selection': '',
+                        'eagli_parameters.perspectives': 'Any',
+                        'eagli_parameters.custom_query_string': '',
+                        'field_parameters.start_date': start_date,
+                        'field_parameters.end_date': end_date,
+                        'field_parameters.plants': '',
+                        'field_parameters.archive_selection': '',
+                        'field_parameters.custom_query_string': '',
+                        'dataset': 'combined_images',
+                        'sample_size': 20
+                    }
+                    data = send_req(
+                        method="POST",
+                        endpoint=API_URL,
+                        url="check",
+                        req_data=req_data,
+                        parameters=None
+                    )
+                    # Process response data
+                    print("returned data is: ", data["message"])
+                    if "error" in data.keys():
+                        plant_row.append(data["error"])
+                    elif "message" in data.keys() and data["message"] == "This query yielded no results.":
+                        plant_row.append(0)
+                    else:
+                        plant_row.append(data["number_files"])
 
-    # Return response data and gui-formatted response
-    """from pandas import json_normalize
-    normalized = json_normalize(sp_month_data)"""
-    with open("../output_sp_by_plant.csv", "w") as f:
-        df.to_csv(f)
+            # Write row
+            df[plant] = plant_row
+
+        # Return response data and gui-formatted response
+        with open("../output_sp_by_plant.csv", "w") as f:
+            df.to_csv(f)
+        """
